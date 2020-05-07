7f5fee6169550b557a995c607abe1d5d8f7b92c3
licencode <licencode@gmail.com>
2020-05-07 14:37:50 +0200

    Add project

---
+++ App.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import { StyleSheet, View, Alert } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
+
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
+import { THEME } from './src/theme'
+
+async function loadApplication() {
+  await Font.loadAsync({
+    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
+  })
+}
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false)
+  const [todoId, setTodoId] = useState(null)
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'Apprendre React Native' }
+  ])
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onError={err => console.log(err)}
+        onFinish={() => setIsReady(true)}
+      />
+    )
+  }
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    const todo = todos.find(t => t.id === id)
+    Alert.alert(
+      "Supprimer l'élément",
+      `Voulez-vous vraiment supprimer "${todo.title}"?`,
+      [
+        {
+          text: 'Annuler',
+          style: 'cancel'
+        },
+        {
+          text: 'Effacer',
+          style: 'destructive',
+          onPress: () => {
+            setTodoId(null)
+            setTodos(prev => prev.filter(todo => todo.id !== id))
+          }
+        }
+      ],
+      { cancelable: false }
+    )
+  }
+
+  const updateTodo = (id, title) => {
+    setTodos(old =>
+      old.map(todo => {
+        if (todo.id === id) {
+          todo.title = title
+        }
+        return todo
+      })
+    )
+  }
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  )
+
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId)
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        goBack={() => setTodoId(null)}
+        todo={selectedTodo}
+        onSave={updateTodo}
+      />
+    )
+  }
+
+  return (
+    <View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>{content}</View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: THEME.PADDING_HORIZONTAL,
+    paddingVertical: 20
+  }
+})
---
+++ src/components/EditModal.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, Button, Modal, Alert } from 'react-native'
+import { THEME } from '../theme'
+import { AppButton } from './ui/AppButton'
+
+export const EditModal = ({ visible, onCancel, value, onSave }) => {
+  const [title, setTitle] = useState(value)
+
+  const saveHandler = () => {
+    if (title.trim().length < 3) {
+      Alert.alert(
+        'Erreur!',
+        `La longueur minimale du nom de la tâche est de 3 caractères. Maintenant ${
+          title.trim().length
+        } les symboles.`
+      )
+    } else {
+      onSave(title)
+    }
+  }
+
+  return (
+    <Modal visible={visible} animationType='slide' transparent={false}>
+      <View style={styles.wrap}>
+        <TextInput
+          value={title}
+          onChangeText={setTitle}
+          style={styles.input}
+          placeholder='Entrez le nom de la tâche'
+          autoCapitalize='none'
+          autoCorrect={false}
+          maxLength={64}
+        />
+        <View style={styles.buttons}>
+          <AppButton onPress={onCancel} color={THEME.DANGER_COLOR}>
+          Annuler
+          </AppButton>
+          <AppButton onPress={saveHandler}>Enregistrer</AppButton>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    padding: 10,
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 2,
+    width: '80%'
+  },
+  buttons: {
+    width: '100%',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+})
